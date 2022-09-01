21936cd3e5e97884604a977caa455525cd25af21
Uriel F <uriellibano2009@gmail.com>
2022-09-01 19:39:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const reverseNumber = (number) => {
+	let reverse = '';
+	for (let i = String(number).length; i > 0; i--) {
+		reverse += String(number)[i - 1];
+	}
+	return reverse;
+};
+
+const getFirstPalindromicNumbers = (entryNumber, digits) => {
+	let numbersInRange = [];
+	const numberArray = Array.from(String(entryNumber), Number);
+	let j = 0;
+	let found = false;
+	while (found == false) {
+        for (let i = 0; i < digits; i++) {
+            if (numberArray[i + j] == undefined)
+                return `Não há palíndromos com ${digits} dígitos dentro do número informado.`;
+            numbersInRange.push(numberArray[i + j]);
+		}
+
+		const number = numbersInRange.toString();
+
+		if (number == reverseNumber(number)) {
+			found = true;
+			return numbersInRange.toString();
+		}
+		j += 1;
+		numbersInRange = [];
+	}
+};
+
+console.log(getFirstPalindromicNumbers(1233334543333, 12))
+
